c6f6af4deac55d589000676d5ea74ca611846596
Bodi <bodiaugustus@outlook.com>
2021-12-12 15:00:01 -0600

    Changed contracts directory into public to get browser access. Set up the loadContract function and used it to start getting deployed contracts inside the browser by using reformatted artifacts.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import React, {useState, useEffect} from 'react';
 import Web3 from 'web3';
 import detectEthereumProvider from '@metamask/detect-provider'
+import {loadContract} from "./utils/load-contract";
 
 
 function App() {
   const [web3Api, setWeb3Api] = useState({
     provider: null,
-    web3: null
+    web3: null,
+    contract: null
   })
 
   const [account, setAccount] = useState(null);
@@ -16,11 +18,14 @@ function App() {
   React.useEffect(() => {
     const loadProvider = async () => {
       const provider = await detectEthereumProvider()
+      const contract = await loadContract("Faucet", provider)
+      debugger
       if(provider){
-        provider.request({method: "eth_requestAccounts"})
+        // provider.request({method: "eth_requestAccounts"})
         setWeb3Api({
           web3: new Web3(provider),
-          provider
+          provider,
+          contract
         })
       }else{
         console.error("Please install metamask.")
@@ -47,16 +52,18 @@ function App() {
         <span>
           <strong>Account : </strong>
         </span>
-        <h1>{account ? account : 'not connected'}</h1>
-          <div className="balance-view is-size-2">
+        <h1>{account ? <div>{account}</div> : <button className='button is-info'
+        onClick={()=> web3Api.provider.request({method: "eth_requestAccounts"})}
+        >Connect Account</button>}</h1>
+          <div className="balance-view is-size-2 mb-4">
             Current Balance: <strong>10</strong> ETH
           </div>
           {/* <button className="btn mr-2" onClick={async () => {
             const accounts = await window.ethereum.request({method: "eth_requestAccounts"})
             console.log(accounts)
           }}>Enable Ethereum</button> */}
-          <button className='btn mr-2'>Donate</button>
-          <button className='btn'>Withdrawl</button>
+          <button className='button is-primary mr-2'>Donate</button>
+          <button className='button is-link'>Withdrawl</button>
         </div>
       </div>
     </>
